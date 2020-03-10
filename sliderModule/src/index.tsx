70068b73a674a3cb8a3a4e9fc90d4d225a03495e
brynjarb15 <brynjar_b@hotmail.com>
2020-03-10 20:04:56 +0000

    Add sliders-phone which will show up in the sliders-phone div

--- sliderModule/src/index.tsx
+++ sliderModule/src/index.tsx
@@ -4,6 +4,7 @@ import App from './app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('sliders'));
+ReactDOM.render(<App />, document.getElementById('sliders-phone'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
