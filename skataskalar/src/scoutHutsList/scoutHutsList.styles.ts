5380dcbc42360e5fa6d55f64f169b6b7e417895d
brynjarb15 <brynjar_b@hotmail.com>
2020-05-14 11:07:30 +0000

    Mostly clean up and bunch of comments and a bit of css changes

--- skataskalar/src/scoutHutsList/scoutHutsList.styles.ts
+++ skataskalar/src/scoutHutsList/scoutHutsList.styles.ts
@@ -32,12 +32,14 @@ export const Title = styled.h1`
 `;
 
 export const ScoutHutItem = styled.div`
-	margin: 0 0 2rem 0;
+	margin: 1rem 0 2rem 0;
 	clear: both;
-	height: 400px;
+	min-height: 200px;
+	display: table;
 `;
 
 export const ClickableContainer = styled.a`
+	display: table-row;
 	color: inherit;
 	text-decoration: none;
 	&:hover {
@@ -58,6 +60,11 @@ export const ScoutHutTitle = styled.h2`
 export const TextWrapper = styled.div`
 	float: left;
 	margin-left: 1rem;
+	@media screen and (min-width: 400px) {
+		min-width: 380px;
+	}
+	
+	
 `;
 
 export const ItemList = styled.ul`
@@ -89,22 +96,58 @@ export const PictureWrapper = styled.div`
 export const Picture = styled.img`
 	width: 100%;
 	object-fit: cover;
+	@media screen and (max-width: 460px) {
+		width: 90%;
+	}
 	
 `;
 
-export const filterWrapper = styled.div`
+export const FilterWrapper = styled.div`
     margin: auto;
     margin-bottom: 3rem;
-    width: 70%;
+    width: 90%;
 `;
 
-export const filterInput = styled.input`
+export const FilterInput = styled.input`
 	margin-left: 1rem;
-	width: 50%;
+	width: 40%;
 	padding: 12px 20px;
 	margin: 8px 0 8px 20px;
 	display: inline-block;
 	border: 1px solid #ccc;
 	border-radius: 4px;
 	box-sizing: border-box;
+	@media screen and (max-width: 670px) {
+		width: 90%;
+	}
 `;
+
+export const FilterLabel = styled.label`
+	
+`;
+
+export const FilterSliderSuperWrapper = styled.div`
+`;
+
+export const FilterSliderWrapper = styled.div`
+	float: left;
+	width: 74%;
+	@media screen and (max-width: 670px) {
+		width: 120%;
+	}
+`;
+
+
+export const FilterSliderText = styled.div`
+	float: left;
+	margin-top: 0.7rem;
+	margin-right: 2rem;
+`;
+
+export const BreakLine = styled.hr`
+	width: 78%;
+	margin-right: auto !important;
+	margin-left :auto !important;
+    margin-bottom: 4rem;
+	color: #DCDCDC;
+	`;
